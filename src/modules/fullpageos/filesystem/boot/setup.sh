df185d29a843f27015893280f313de16c37d7e4b
k-bunge <k.bunge@abasoft.de>
2025-12-05 09:22:47 +0100

    [EVAV3-1179] included tts packages to start script and setup.sh for setting up the landing page

---
+++ src/modules/fullpageos/filesystem/boot/setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#User Input
+read -p "Bitte geben Sie den Token ein: " token
+if [ -z "$token" ]; then
+  echo "Token darf nicht leer sein!"
+  exit 1
+fi
+
+read -p "Bitte geben Sie die IP-Adresse ein: " ip_address
+if [ -z "$ip_address" ]; then
+  echo "IP-Adresse darf nicht leer sein!"
+  exit 1
+fi
+
+# API Abfragen
+full_api_url="http://${ip_address}:8000/extern/wrm/api/waitingroom-monitor/generate-link?token=${token}"
+response=$(curl -s "$full_api_url")
+
+if [ -z "$response" ]; then
+  echo "Keine Antwort vom Server erhalten. Überprüfen Sie die API-URL oder den Token."
+  exit 1
+fi
+
+link=$(echo "$response" | grep -oP '"link":\s*"\K[^"]+')
+
+if [ -z "$link" ]; then
+  echo "Konnte die 'link'-Information aus der API-Antwort nicht extrahieren."
+  exit 1
+fi
+
+#WZM-Link erstellen
+final_link="http://${ip_address}:3000${link}"
+echo "Der generierte Link ist: $final_link"
+
+#Config-Datei überschreiben
+config_file="/boot/firmware/fullpageos.txt"
+
+if [ ! -f "$config_file" ]; then
+  echo "Konfigurationsdatei $config_file wurde nicht gefunden."
+  exit 1
+fi
+
+echo "$new_link" > "$target_file"
+
+if grep -q "$final_link" "$config_file"; then
+  echo "Die Datei wurde erfolgreich mit dem neuen Link überschrieben."
+else
+  echo "Ein Fehler ist beim Überschreiben der Datei aufgetreten."
+  exit 1
+fi
